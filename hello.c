9991acd814a4db8d3cc2cde6977c6762aad3fbdf
KMai6 <54297920+KMai6@users.noreply.github.com>
2019-08-29 18:45:50 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Kevin Mai
+ * Date: 2019/29/08
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Kevin Mai\n");
+
+  return 0;
+
+}
